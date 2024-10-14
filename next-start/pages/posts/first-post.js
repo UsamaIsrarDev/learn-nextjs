6d8e380f8ad5dc03a35bd863d116b64e35e3c108
Usama Israr <156895405+Usama1190@users.noreply.github.com>
2024-10-14 08:40:33 -0700

    Navigation and editing done on Oct 14, 24

--- next-start/pages/posts/first-post.js
+++ next-start/pages/posts/first-post.js
@@ -5,7 +5,7 @@ export default function FirstPost() {
   return (
     <div>
         <h1>First Post</h1>
-        <h2><Link href='/'>Got to home page</Link></h2>
+        <h2><Link href='/'>Back to home</Link></h2>
     </div>
   )
 }
