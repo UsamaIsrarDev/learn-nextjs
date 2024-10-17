9bc0b06ef40fc814e6d6d102174707cc0f79273b
Usama Israr <156895405+Usama1190@users.noreply.github.com>
2024-10-17 05:03:11 -0700

    Polishing layout in project completed then move to the data fetching topics on Oct 17, 24

--- next-start/pages/posts/first-post.js
+++ next-start/pages/posts/first-post.js
@@ -1,27 +1,14 @@
-import Link from 'next/link'
-import React from 'react'
-import Head from 'next/head'
-import Script from 'next/script'
-import Layout from '../../components/layout'
+import Head from 'next/head';
+import Link from 'next/link';
+import Layout from '../../components/layout';
 
 export default function FirstPost() {
   return (
     <Layout>
       <Head>
         <title>First Post</title>
-        <script src="https://connect.facebook.net/en_US/sdk.js" />
       </Head>
-
-      <Script
-        src="https://connect.facebook.net/en_US/sdk.js"
-        strategy="lazyOnload"
-        onLoad={() =>
-          console.log(`script loaded correctly, window.FB has been populated`)
-        }
-      />
-
       <h1>First Post</h1>
-      <h2><Link href='/'>⬅ Back to home</Link></h2>
     </Layout>
-  )
+  );
 }
