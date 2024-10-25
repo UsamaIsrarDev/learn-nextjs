d145f4efe8d506aea85afc623b19c72cb2c55c50
Usama Israr <156895405+Usama1190@users.noreply.github.com>
2024-10-25 01:55:50 -0700

    Completed tailwind-stater on Oct 25, 24

--- tailwind-starter/app/page.tsx
+++ tailwind-starter/app/page.tsx
@@ -6,15 +6,15 @@ export default function Home() {
     //   Hello world!
     // </h1>
 
-    <div className="flex justify-center">
+    <div className="flex justify-center bg-sky-700">
       {""}
-      <div className="mx-4 order-last">
+      <div className="m-4 order-last">
         <Image src={'/profile.jpg'} alt="Next Logo" width={100} height={100}/>
       </div>
-      <div className="mx-4 self-center">
-        <h1>Usama Israr Khan</h1>
-        <h2>A premium in sight and sound</h2>
-        <button>Learn more</button>
+      <div className="mx-4 self-center text-center">
+        <h1 className="text-3xl font-bold text-zinc-200">Usama Israr Khan</h1>
+        <h2 className="text-1xl font-semibold text-zinc-200">Teaching Web 3 & Metaverse classes</h2>
+        <button className="my-4 px-4 py-2 border-2 border-zinc-200 rounded-lg text-zinc-200">Learn more</button>
       </div>
     </div>
   );
