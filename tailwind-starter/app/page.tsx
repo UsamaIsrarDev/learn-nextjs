90185dba7e2f0f0116548e2b470fc7fea01d1310
Usama Israr <156895405+Usama1190@users.noreply.github.com>
2024-10-25 01:30:21 -0700

    Paste image and use help ful classes which is utility classes of tailwind css and get more about in the next class on Oct 25, 24

--- tailwind-starter/app/page.tsx
+++ tailwind-starter/app/page.tsx
@@ -1,7 +1,21 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <h1 className="text-6xl font-bold underline">
-      Hello world!
-    </h1>
+    // <h1 className="text-6xl font-bold underline">
+    //   Hello world!
+    // </h1>
+
+    <div className="flex justify-center">
+      {""}
+      <div className="mx-4 order-last">
+        <Image src={'/profile.jpg'} alt="Next Logo" width={100} height={100}/>
+      </div>
+      <div className="mx-4 self-center">
+        <h1>Usama Israr Khan</h1>
+        <h2>A premium in sight and sound</h2>
+        <button>Learn more</button>
+      </div>
+    </div>
   );
 }
